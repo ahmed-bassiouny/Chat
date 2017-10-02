25a54568d8ded0b79dd65832006b82744c66d38d
ahmed <ahmedbassiouny26@gmail.com>
2017-10-02 21:52:02 +0200

    rate functionality

--- app/src/main/java/com/example/ahmed/chat/activity/RateActivity.java
+++ app/src/main/java/com/example/ahmed/chat/activity/RateActivity.java
@@ -2,15 +2,66 @@ package com.example.ahmed.chat.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
 
 import com.example.ahmed.chat.R;
+import com.example.ahmed.chat.helper.Constants;
+import com.example.ahmed.chat.model.MyAccount;
+import com.example.ahmed.chat.model.User;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class RateActivity extends AppCompatActivity {
 
+    String userId;
+    double currentRate;
+    int numberOfChat;
+    RatingBar rating;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rate);
+        rating = (RatingBar) findViewById(R.id.rating);
         this.setFinishOnTouchOutside(false);
+        if(MyAccount.getId().equals(getIntent().getStringExtra(Constants.FIRST_PERSON_KEY))){
+            userId = getIntent().getStringExtra(Constants.SECOND_PERSON_KEY);
+        }else {
+            userId = getIntent().getStringExtra(Constants.FIRST_PERSON_KEY);
+        }
+        getUserData();
+        FirebaseDatabase.getInstance().getReference(Constants.USER).child(userId).child("chatWith").setValue((numberOfChat+1));
+        findViewById(R.id.btnSave).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                currentRate += rating.getRating();
+                FirebaseDatabase.getInstance().getReference(Constants.USER).child(userId).child("rate").setValue(currentRate);
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+
+    }
+
+    private void getUserData(){
+        FirebaseDatabase.getInstance().getReference(Constants.USER).child(userId).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                User user = dataSnapshot.getValue(User.class);
+                currentRate = user.rate;
+                numberOfChat = user.chatWith;
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
